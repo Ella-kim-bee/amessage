f4bd05232a88aa6bb5cfc18e7ced6e1232a38e28
Ella-kim-bee <a20281013@gmail.com>
2022-02-10 20:48:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# amessage
+# amessage
---
+++ js/common.js
@@ -0,0 +1,44 @@
+// ========== menu toggle ==========
+let btnToggle = document.getElementById("btnToggle");
+let $header = document.getElementById("header");
+let $span = document.querySelector("button#btnToggle > span");
+
+btnToggle.addEventListener('click', function(e){
+    let e2 = e ? e : window.event;
+$header.classList.toggle('on');
+    if($header.classList.contains('on')){
+        $span.innerHTML = "&Cross;";
+    }
+    else{
+        $span.innerHTML = "&equiv;";
+    }        
+});
+
+// ========== button.chkBox toggle ==========
+let btnChkBox = document.querySelectorAll(".chkBox");
+
+for(let i = 0; i < btnChkBox.length; i++){
+  btnChkBox[i].addEventListener('click', function(e){
+    let e2 = e ? e : window.event;
+    btnChkBox[i].classList.toggle('chk');
+  });
+}
+
+// if(btnChkBox[0].classList.contains("chk")){
+//   btnChkBox
+// }
+
+
+// ========== button.star toggle ==========
+let btnStar = document.querySelectorAll(".star");
+
+for(let i = 0; i < btnChkBox.length; i++){
+  btnStar[i].addEventListener('click', function(e){
+    let e2 = e ? e : window.event;
+    btnStar[i].classList.toggle('chk');
+  });
+}
+
+        
+        
+
